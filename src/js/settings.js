75c79bca6d7826aed0bef0cc8a7301bf89c63ddb
G07cha <azizovkostya97@gmail.com>
2015-10-03 11:34:15 +0300

    Add saving to settings
    Now on click 'Save' button it will save work/relax slider and launch on startup state and restore it on reloading page. If no settings found, will be used default.

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,23 +1,66 @@
 var remote = require('remote');
 var dialog = remote.require('dialog');
+var app = remote.require('app');
 var browserWindow = remote.require('browser-window');
 window.$ = window.jQuery = require('jquery');
+var fs = require('fs');
 
-workTimer = 1500000;
-relaxTimer = 300000;
+var workTimer = 25;
+var relaxTimer = 5;
+var launchOnStartup = false;
+try {
+	var data = JSON.parse(fs.readFileSync(app.getDataPath() + '/config.json'));
+	workTimer = data.workTimer;
+	relaxTimer = data.relaxTimer;
+	launchOnStartup = data.launchOnStartup;
+} catch(err) {
+	console.log('Didn\'t found previous config. Using default settings');
+}
 
 $(document).ready(function() {
-	$('.workTimer').attr('value', workTimer/1000/60);
-	$('.relaxTimer').attr('value', relaxTimer/1000/60);
-	$('.workValue').append(workTimer/1000/60);
-	$('.relaxValue').append(relaxTimer/1000/60);
+	slider('work', workTimer);
+	slider('relax', relaxTimer);
+	$('.launch').attr('checked', launchOnStartup);
 	
-	$('.workTimer').change(function() {
-		$('.workValue').empty();
-		$('.workValue').append($(this).val());
+	$('.save').on('click', function() {
+		fs.writeFile(app.getDataPath() + '/config.json', JSON.stringify({
+			workTimer: $('.workTimer').val(),
+			relaxTimer: $('.relaxTimer').val(),
+			launchOnStartup: $('.launch').prop('checked')
+		}), function(err) {
+			if (err) {
+				dialog.showErrorBox('Failed to save settings', err);
+			} else {
+				dialog.showMessageBox({
+					title: 'Success',
+					message: 'Changes saved successfully!',
+					buttons: ['Ok']
+				});
+			}
+		});
 	});
-	$('.relaxTimer').change(function() {
-		$('.relaxValue').empty();
-		$('.relaxValue').append($(this).val());
+	
+	$('.cancel').on('click', function() {
+		dialog.showMessageBox({
+			type: 'question',
+			title: 'Warning',
+			message: 'Settings will not save! Are you sure?',
+			buttons: ['Yes', 'No']
+		}, function(response) {
+			if(response === 0) {
+				//Exit from settings
+			}
+		})
 	});
 });
+
+function slider(name, value) {
+	var timerSelector = '.' + name + 'Timer';
+	var valueSelector = timerSelector.replace('Timer', 'Value');
+	$(timerSelector).attr('value', value);
+	$(valueSelector).append(value);
+	$(timerSelector).change(function() {
+		$(valueSelector).empty();
+		$(valueSelector).append($(this).val());
+	});
+}
