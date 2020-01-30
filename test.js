a16de89314f4a51d511971378722618a70acc3a3
Gianfranco Palumbo <gianpa@gmail.com>
2020-01-30 13:47:23 +0000

    add prettier and format test.js

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-import test from 'ava';
-import {Application} from 'spectron';
+import test from "ava";
+import { Application } from "spectron";
 
 test.beforeEach(async t => {
   t.context.app = new Application({
-    path: './dist/pomodoro-darwin-x64/pomodoro.app/Contents/MacOS/Pomodoro'
+    path: "./dist/pomodoro-darwin-x64/pomodoro.app/Contents/MacOS/Pomodoro"
   });
 
   await t.context.app.start();
@@ -13,73 +13,93 @@ test.afterEach.always(async t => {
   await t.context.app.stop();
 });
 
-test('Start/stop timer', async t => {
+test("Start/stop timer", async t => {
   const app = t.context.app;
-  const {client} = app;
+  const { client } = app;
 
   await client.waitUntilWindowLoaded();
 
   const win = app.browserWindow;
 
-  t.false(await win.isVisible(), 'Window isn\'t hidden on start');
+  t.false(await win.isVisible(), "Window isn't hidden on start");
 
-  const {width, height} = await win.getBounds();
+  const { width, height } = await win.getBounds();
 
   t.true(width > 0);
   t.true(height > 0);
 
-  t.is(await client.getText('.timer > strong'), 'Click to start', 'Timer doesn\'t show text on start');
-  await client.leftClick('.timer');
-  t.not(await client.getText('.timer > strong'), 'Click to start', 'Timer\'s text isn\'t changed after start');
+  t.is(
+    await client.getText(".timer > strong"),
+    "Click to start",
+    "Timer doesn't show text on start"
+  );
+  await client.leftClick(".timer");
+  t.not(
+    await client.getText(".timer > strong"),
+    "Click to start",
+    "Timer's text isn't changed after start"
+  );
 
   const timerData = await client.execute(function() {
-    return $('.timer').circleProgress('value');
+    return $(".timer").circleProgress("value");
   });
 
-  t.true(timerData.value > 0, 'Timer isn\'t started');
-  await client.leftClick('.timer');
-  t.is(await client.getText('.timer > strong'), 'Click to start', 'Timer is not stopped after click');
+  t.true(timerData.value > 0, "Timer isn't started");
+  await client.leftClick(".timer");
+  t.is(
+    await client.getText(".timer > strong"),
+    "Click to start",
+    "Timer is not stopped after click"
+  );
 });
 
-test('Reset timer', async t => {
+test("Reset timer", async t => {
   const app = t.context.app;
-  const {client} = app;
+  const { client } = app;
 
   await client.waitUntilWindowLoaded();
 
-  await client.leftClick('.timer');
-  t.not(await client.getText('.timer > strong'), 'Click to start', 'Timer\'s text isn\'t changed after start');
+  await client.leftClick(".timer");
+  t.not(
+    await client.getText(".timer > strong"),
+    "Click to start",
+    "Timer's text isn't changed after start"
+  );
 
-  await client.leftClick('#resetBtn');
-  await client.waitUntilTextExists('.timer > strong', 'Click to start', 1000);
-  t.is(await client.getText('.timer > strong'), 'Click to start', 'Timer is not stopped after reset');
+  await client.leftClick("#resetBtn");
+  await client.waitUntilTextExists(".timer > strong", "Click to start", 1000);
+  t.is(
+    await client.getText(".timer > strong"),
+    "Click to start",
+    "Timer is not stopped after reset"
+  );
 
   const timerData = await client.execute(function() {
-    return $('.timer').circleProgress('value');
+    return $(".timer").circleProgress("value");
   });
 
-  t.true(timerData.value === 0, 'Timer value isn\'t resetted');
+  t.true(timerData.value === 0, "Timer value isn't resetted");
 });
 
-test('Open settings', async t => {
+test("Open settings", async t => {
   const app = t.context.app;
-  const {client} = app;
+  const { client } = app;
 
   await client.waitUntilWindowLoaded();
 
-  await client.leftClick('#settingsBtn');
-  t.is(await app.client.getWindowCount(), 2, 'Setting window is missing');
+  await client.leftClick("#settingsBtn");
+  t.is(await app.client.getWindowCount(), 2, "Setting window is missing");
 });
 
-test('Main window accessibility', async t => {
+test("Main window accessibility", async t => {
   const app = t.context.app;
 
   app.client.windowByIndex(0).then(function() {
-    app.client.auditAccessibility().then(function (audit) {
+    app.client.auditAccessibility().then(function(audit) {
       if (audit.failed) {
-        console.error('Main page failed audit')
-        console.error(audit.message)
+        console.error("Main page failed audit");
+        console.error(audit.message);
       }
-    })
-  })
-})
+    });
+  });
+});
