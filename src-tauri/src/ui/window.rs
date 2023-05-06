a83df91af0f972b87e308848f54c41deb89c0f46
Konstantin Azizov <azizovkostya97@gmail.com>
2023-05-06 18:44:45 +0200

    feat: imporove main window background on Windows
    Follow system's theme and adjust for better contrast with inner elements.

--- src-tauri/src/ui/window.rs
+++ src-tauri/src/ui/window.rs
@@ -1,5 +1,3 @@
-#[cfg(target_os = "windows")]
-use window_vibrancy::apply_blur;
 #[cfg(target_os = "macos")]
 use window_vibrancy::{apply_vibrancy, NSVisualEffectMaterial};
 
@@ -10,12 +8,35 @@ pub fn decorate_window(window: &Window) {
   apply_vibrancy(
     window,
     NSVisualEffectMaterial::Popover,
-    Some(window_vibrancy::NSVisualEffectState::Active),
+    Some(window_vibrancy::NSVisualEffectState::FollowsWindowActiveState),
     Some(8.0),
   )
   .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
 
   #[cfg(target_os = "windows")]
-  apply_blur(window, Some((18, 18, 18, 125)))
-    .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+  {
+    use tauri::{Theme, WindowEvent};
+    use window_vibrancy::apply_acrylic;
+
+    fn apply_windows_theme(theme: &Theme, window: &Window) {
+      match theme {
+        Theme::Light => apply_acrylic(window, Some((255, 255, 255, 125)))
+          .expect("Unsupported platform! 'apply_acrylic' is only supported on Windows"),
+        Theme::Dark => apply_acrylic(window, Some((0, 0, 0, 50)))
+          .expect("Unsupported platform! 'apply_acrylic' is only supported on Windows"),
+        _ => todo!(),
+      }
+    }
+
+    apply_windows_theme(&window.theme().unwrap(), window);
+
+    window.on_window_event({
+      let window = window.clone();
+      move |event| {
+        if let WindowEvent::ThemeChanged(theme) = event {
+          apply_windows_theme(theme, &window)
+        }
+      }
+    });
+  }
 }
