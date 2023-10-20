c2bd324292b9cd73d4a0819f520e76b94f65ad15
Konstantin Azizov <azizovkostya97@gmail.com>
2023-10-20 18:52:35 +0200

    fix: remove TAURI_ env variables from frontend bundle
    Rotate updater key just in case

--- src-ui/vite.config.ts
+++ src-ui/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   // to make use of `TAURI_PLATFORM`, `TAURI_ARCH`, `TAURI_FAMILY`,
   // `TAURI_PLATFORM_VERSION`, `TAURI_PLATFORM_TYPE` and `TAURI_DEBUG`
   // env variables
-  envPrefix: ['VITE_', 'TAURI_'],
+  envPrefix: ['VITE_'],
   build: {
     // Tauri uses Chromium on Windows and WebKit on macOS and Linux
     target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari15',
